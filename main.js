293886068cdc3b55c1601a092582cd34c85a99b6
dadaa <dadaa@users.noreply.github.com>
2019-10-26 23:37:43 +0900

    use setProperty instead

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const enlargedElement = document.querySelector("#pnnext span:nth-child(2)");
-enlargedElement.style.transform = "scale(16)";
-enlargedElement.style.transformOrigin = "left bottom";
+const element = document.querySelector("#pnnext span:nth-child(2)");
+element.style.setProperty("transform", "scale(16)");
+element.style.setProperty("transform-origin", "left bottom");
